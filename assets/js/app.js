7b8c6a977ac664c5fa36ffa4391dced86adff701
JORGE <jorge.molina.galdames@gmail.com>
2022-01-03 18:49:33 -0300

    hola

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+$("document").ready(() => {
+  
+  // Carousel
+  $(".carousel").carousel({
+    interval: 3000,
+  });
+
+  $(".carousel-control-prev").click(function (e) {
+    e.preventDefault();
+    $("#pvCarousel").carousel("prev");
+  });
+
+  $(".carousel-control-next").click(function (e) {
+    e.preventDefault();
+    $("#pvCarousel").carousel("next");
+  });
+
+  // Scroll de Menu
+  $(".navbar a").click(function (e) {
+    e.preventDefault();
+    const goto = this.hash;
+    $("html").animate(
+      {
+        scrollTop: $(goto).offset().top,
+      },
+      600
+    );
+  });
+
+  // Tooltip en cards
+  $('[data-toggle="tooltip"]').tooltip();
+});
